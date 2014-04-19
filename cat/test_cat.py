07ec9800ccad284d31187e8748a97ab118945141
Gregory Eric Sanderson <gzou2000@gmail.com>
2014-04-19 19:06:31 -0400

    cat - DRY tests: refactor file creation

--- cat/test_cat.py
+++ cat/test_cat.py
@@ -1,4 +1,23 @@
+import pytest
+
 from cat import cat
+
+
+class FileMaker(object):
+
+    def __init__(self, tmpdir):
+        self.tmpdir = tmpdir
+
+    def create_file(self, filename, contents):
+        filepath = self.tmpdir.join(filename)
+        filepath.write(contents)
+        return str(filepath)
+
+
+@pytest.fixture
+def filemaker(tmpdir):
+    return FileMaker(tmpdir)
+
 
 def test_given_wrong_file_name_then_prints_error_message(capsys):
     cat('superduperfilename')
@@ -7,34 +26,30 @@ def test_given_wrong_file_name_then_prints_error_message(capsys):
     assert err == "cat: superduperfilename: No such file or directory"
 
 
-def test_given_empty_file_then_prints_newline(capsys, tmpdir):
-    filepath = tmpdir.join('emptyfile')
-    filepath.write('')
+def test_given_empty_file_then_prints_newline(capsys, filemaker):
+    filepath = filemaker.create_file("emptyfile", "")
 
-    cat(str(filepath))
+    cat(filepath)
 
     out, err = capsys.readouterr()
     assert out == "\n"
 
 
-def test_given_single_file_then_prints_newline(capsys, tmpdir):
-    filepath = tmpdir.join('poem')
-    filepath.write("The quick brown fox jumps over the lazy dog")
+def test_given_single_file_then_prints_newline(capsys, filemaker):
+    filepath = filemaker.create_file("poem",
+        "The quick brown fox jumps over the lazy dog")
 
-    cat(str(filepath))
+    cat(filepath)
 
     out, err = capsys.readouterr()
     assert out == "The quick brown fox jumps over the lazy dog\n"
 
 
-def test_given_multiple_files_then_prints_them_in_order(capsys, tmpdir):
-    first_file = tmpdir.join('firstfile')
-    first_file.write('Roses are red')
+def test_given_multiple_files_then_prints_them_in_order(capsys, filemaker):
+    first_file = filemaker.create_file("firstfile", "Roses are red")
+    second_file = filemaker.create_file("secondfile", "Violets are blue")
 
-    second_file = tmpdir.join('secondfile')
-    second_file.write('Violets are blue')
-
-    cat(str(first_file), str(second_file))
+    cat(first_file, second_file)
 
     out, err = capsys.readouterr()
     assert out == "Roses are red\nViolets are blue\n"
