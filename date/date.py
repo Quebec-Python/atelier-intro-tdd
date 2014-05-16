77722463430809cc050b7d9d9329ebcc7df26beb
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-05-16 16:00:27 -0400

    date - refactor tests and add all basic fields

--- date/date.py
+++ date/date.py
@@ -1,5 +1,10 @@
 def date(timestamp, pattern):
     output = (pattern
               .replace('%%', '%')
-              .replace('%Y', str(timestamp.year)))
+              .replace('%Y', str(timestamp.year))
+              .replace('%m', str(timestamp.month))
+              .replace('%d', str(timestamp.day))
+              .replace('%H', str(timestamp.hour))
+              .replace('%M', str(timestamp.minute))
+              .replace('%S', str(timestamp.second)))
     print(output)
--- date/test_date.py
+++ date/test_date.py
@@ -4,6 +4,7 @@ from date import date
 
 TIMESTAMP = datetime.datetime(2001, 2, 3, 4, 5, 6)
 
+
 def test_given_no_format_then_prints_newline(capsys):
     date(TIMESTAMP, '')
 
@@ -25,8 +26,18 @@ def test_given_format_with_double_percent_then_prints_percent(capsys):
     assert out == '%\n'
 
 
-def test_given_format_with_full_year_then_prints_full_year(capsys):
-    date(TIMESTAMP, '%Y')
+def test_given_full_date_then_prints_all_fields(capsys):
+    fields = {
+        '%Y': TIMESTAMP.year,
+        '%m': TIMESTAMP.month,
+        '%d': TIMESTAMP.day,
+        '%H': TIMESTAMP.hour,
+        '%M': TIMESTAMP.minute,
+        '%S': TIMESTAMP.second
+    }
 
-    out, err = capsys.readouterr()
-    assert out == '2001\n'
+    for fmt, field in fields.items():
+        date(TIMESTAMP, fmt)
+
+        out, err = capsys.readouterr()
+        assert str(field) in out
