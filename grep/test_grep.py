2d7a0c99d5500c35e926e20ddf9647a22c2cf4d3
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-05-16 15:01:11 -0400

    grep - test 4: refactor tests, make them DRY

--- grep/test_grep.py
+++ grep/test_grep.py
@@ -1,45 +1,57 @@
+import pytest
+
 from grep import grep
 
 
-def test_given_empty_file_when_empty_pattern_then_prints_nothing(capsys, tmpdir):
-    path = tmpdir.join('emptyfile')
-    path.write('')
+class FileUtil(object):
 
-    grep('', str(path))
+    def __init__(self, tmpdir):
+        self.tmpdir = tmpdir
+
+    def create_file(self, name, contents):
+        path = self.tmpdir.join(name)
+        path.write(contents)
+        return str(path)
+
+
+@pytest.fixture
+def fileutil(tmpdir):
+    return FileUtil(tmpdir)
+
+
+def test_given_empty_file_when_empty_pattern_then_prints_nothing(capsys, fileutil):
+    path = fileutil.create_file('emptyfile', '')
+
+    grep('', path)
 
     out, err = capsys.readouterr()
     assert out == "\n"
 
 
-def test_given_file_with_a_line_when_empty_pattern_then_prints_line(capsys, tmpdir):
-    path = tmpdir.join('oneline')
-    path.write('one line')
+def test_given_file_with_a_line_when_empty_pattern_then_prints_line(capsys, fileutil):
+    path = fileutil.create_file('oneline', 'one line')
 
-    grep('', str(path))
+    grep('', path)
 
     out, err = capsys.readouterr()
     assert 'one line' in out
 
 
-def test_given_file_with_two_lines_when_pattern_contains_a_line_then_prints_line(capsys, tmpdir):
-    path = tmpdir.join('twolines')
-    path.write('a\nb\n')
+def test_given_file_with_two_lines_when_pattern_contains_a_line_then_prints_line(capsys, fileutil):
+    path = fileutil.create_file('twolines', 'a\nb\n')
 
-    grep('b', str(path))
+    grep('b', path)
 
     out, err = capsys.readouterr()
     assert 'b' in out
     assert 'a' not in out
 
 
-def test_given_2_files_when_grepping_then_prints_lines_in_all_files(capsys, tmpdir):
-    first_file = tmpdir.join('first')
-    first_file.write('aa\nbb\n')
+def test_given_2_files_when_grepping_then_prints_lines_in_all_files(capsys, fileutil):
+    first_file = fileutil.create_file('first', 'aa\nbb\n')
+    second_file = fileutil.create_file('second', 'ab\nbc\n')
 
-    second_file = tmpdir.join('second')
-    second_file.write('ab\nbc\n')
-
-    grep('a', str(first_file), str(second_file))
+    grep('a', first_file, second_file)
 
     out, err = capsys.readouterr()
     assert 'aa' in out
