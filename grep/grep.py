a06ad92a03f6a0dd27543c56e2e27f034223a7d7
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-05-16 15:33:34 -0400

    grep - test 7: refactor grep, make smaller functions

--- grep/grep.py
+++ grep/grep.py
@@ -2,28 +2,27 @@ import re
 
 
 def grep(pattern, *paths, **options):
-    flags = compile_flags(options)
-    regex = re.compile(pattern, flags)
-    matching = []
+    regex = compile_regex(pattern, options)
+    matching = find_matching_lines(regex, paths)
+    lines = format_lines(matching, options)
+    print('\n'.join(lines))
 
+
+def compile_regex(pattern, options):
+    flags = 0
+    if options.get('ignore_case', False):
+        flags |= re.I
+
+    return re.compile(pattern, flags)
+
+
+def find_matching_lines(regex, paths):
+    matching = []
     for path in paths:
         with open(path) as reader:
             matching.extend(filter_matching_lines(regex, reader))
 
-    if options.get('line_number', False):
-        lines = ['{}:{}'.format(line_number, line)
-                 for (line_number, line) in matching]
-    else:
-        lines = [line for (line_number, line) in matching]
-
-    print('\n'.join(lines))
-
-
-def compile_flags(options):
-    flags = 0
-    if options.get('ignore_case', False):
-        flags |= re.I
-    return flags
+    return matching
 
 
 def filter_matching_lines(regex, reader):
@@ -33,3 +32,11 @@ def filter_matching_lines(regex, reader):
             matching.append((line_number, line))
 
     return matching
+
+
+def format_lines(matching, options):
+    template = '{line}'
+    if options.get('line_number', False):
+        template = '{number}:{line}'
+
+    return [template.format(number=number, line=line) for (number, line) in matching]
