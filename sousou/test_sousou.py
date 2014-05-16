34f9c2bb4bfe2476da96a5bf065bb6f5fa1bc8cc
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-05-16 11:12:32 -0400

    sousou - step 1: write another failing test

--- sousou/test_sousou.py
+++ sousou/test_sousou.py
@@ -6,3 +6,10 @@ def test_given_number_one_when_translated_then_prints_kere(capsys):
 
     out, err = capsys.readouterr()
     assert "kere" in out
+
+
+def test_given_number_two_when_translated_then_prints_firi(capsys):
+    translate_number(2)
+
+    out, err = capsys.readouterr()
+    assert "firi" in out
